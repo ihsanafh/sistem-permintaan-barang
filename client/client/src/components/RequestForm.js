11aa985842e624e67ebb30142718d9233e396028
ihsanafh <ahmadi.ihsan3380@student.unri.ac.id>
2025-10-21 00:15:23 +0700

    Fix: perbaikan tampilan dashboard user 2

--- client/client/src/components/RequestForm.js
+++ client/client/src/components/RequestForm.js
@@ -84,7 +84,7 @@ function RequestForm({ onNewRequest }) {
         <div className="form-row">
           <div className="form-group">
             <label>Nama Lengkap</label>
-            <input type="text" value={user.fullname} disabled />
+            <input type="text" value={user.fullName} disabled />
           </div>
           <div className="form-group">
             <label htmlFor="department">Ruangan/Departemen</label>
